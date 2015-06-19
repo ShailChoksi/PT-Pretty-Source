e6be87e905af382137c0f5fb98d55243eb201dea
Shail Choksi <sschoksi@uwaterloo.ca>
2015-06-19 04:03:30 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,145 @@
+var pledgeUrl = "https://kickstarter.getpebble.com/pledges/";
+var inputUrl;
+
+function checkURL() {
+    var url = document.getElementById("URL").value;
+    var regEx = new RegExp(/(https:\/\/)?(kickstarter\.getpebble\.com\/pledges\/)[^/\\]*/);
+
+    if(regEx.test(url)){
+        inputUrl = url;
+        request(url, parsePageHtml);
+    } else {
+        var info = document.getElementById("Info");
+        info.innerHTML = "I know you want your Pebble Time (Steel) so enter the URL correctly!";
+    }
+}
+var request = function(url, callback)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState != 4) {
+            return;
+        }
+        callback(xhr);
+    };
+    xhr.send();
+};
+
+var parsePageHtml = function(result){
+    if(result.status == 200)
+    {
+        chrome.storage.sync.get("pebbleUrl", function(item){
+            if(chrome.runtime.error) TeamNull(chrome.runtime.error);
+            if(!item){
+                chrome.storage.sync.set({'pebbleUrl': inputUrl});
+            }
+        });
+        var parser = new DOMParser();
+        var htmlDoc = parser.parseFromString(result.responseText, "text/html");
+        var jsonEl = htmlDoc.querySelector('[data-react-class="OrdersContainer"]');
+
+        var jsonData = jsonEl.dataset.reactProps;
+        var pledgeData = JSON.parse(jsonData);
+
+        var name = pledgeData.pledge.name;
+
+        var timeTrackingNumber = pledgeData.order.time_tracking_number;
+        var steelTrackingNumber = pledgeData.order.steel_tracking_number;
+
+        //timeTrackingNumber = "q0498t"; //debugging
+
+        if(timeTrackingNumber != null || steelTrackingNumber != null) {
+            ItIsTime(pledgeData.pledge.name, pledgeData.order);
+        }
+        else {
+            TeamNull(null);
+        }
+    }else {
+        TeamNull(result.status);
+    }
+};
+
+var ItIsTime = function(name, data){
+    document.getElementById("PebbleTimeInput").style.display = "none";
+    var ptEl = document.getElementById("PT");
+    var nameEl = document.createElement("div");
+    var nameText = document.createTextNode("Hi " + name + "!");
+    var p = document.createElement("p");
+    var a = document.createElement("a");
+    nameEl.appendChild(nameText);
+
+    ptEl.appendChild(nameEl);
+
+    var dataEl = document.createElement("p");
+    var ttNumber = data.time_tracking_number;
+    var stNumber = data.steel_tracking_number;
+    var ttUrl = data.time_tracking_url;
+    var stUrl = data.steel_tracking_url;
+
+    if(ttNumber != null)
+    {
+        var info = document.createElement("p");
+        var text = document.createTextNode("Your Pebble Time has shipped! Your tracking number is " + ttNumber + ". Click the url below to go to the tracking page:");
+        info.appendChild(text);
+        ptEl.appendChild(info);
+        a.setAttribute("href", ttUrl);
+        a.innerHTML = "Its Time!";
+        ptEl.appendChild(p);
+        ptEl.appendChild(a);
+    }
+
+    if(stNumber != null)
+    {
+        var info = document.createElement("p");
+        info.createTextNode("Your Pebble Time Steel has shipped!");
+        ptEl.appendChild(info);
+        p.createTextNode("Your tracking number is " + stNumber + ". Click the url below to go to the tracking page:");
+        a.setAttribute("href", ttUrl);
+        a.innerHTML = "Its Time!";
+        ptEl.appendChild(p);
+        ptEl.appendChild(a);
+    }
+
+    document.body.appendChild(ptEl);
+
+};
+
+var TeamNull = function(err){
+
+    var input = document.getElementById("PebbleTimeInput");
+    var p = document.createElement("p");
+
+    if(err){
+        p.innerHTML = "Sorry, the extension could not connect to your Pledge page! (Or I F'ed up!). The error is:" + err
+
+    } else {
+        p.innerHTML = "#TeamNull!";
+    }
+
+    input.appendChild(p);
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("PebbleTimeInput").style.display = "none";
+    chrome.storage.sync.get("pebbleUrl", function(item){
+        if(chrome.runtime.error){console.log(chrome.runtime.error);}
+        if(item.pebbleUrl){
+            inputUrl = item.pebbleUrl;
+            request(inputUrl, parsePageHtml);
+        }else{
+            document.getElementById("PebbleTimeInput").style.display = "block";
+            var button = document.getElementById("URLEntered");
+            button.addEventListener("click", checkURL);
+        }
+    });
+});
+
+
+function clearUrl() {
+    chrome.storage.sync.remove("pebbleUrl", function(err){
+        if(chrome.runtime.error) console.log("Error clearing pebble url!", chrome.runtime.error);
+        if(err) console.log(err);
+        console.log("hellow!");
+    });
+}
